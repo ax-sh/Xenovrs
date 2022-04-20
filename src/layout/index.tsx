bbbe2f5216be8aafe07047bc5d6a211c0829309c
Axm <56112475+ax-sh@users.noreply.github.com>
2022-04-20 23:49:56 +0545

    feat: add grid

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -17,7 +17,26 @@ export function List({ children, render, ...props }: ListProps) {
 }
 
 function Footer() {
-  return <footer></footer>;
+  return (
+    <footer className={"bg-yellow p-20 flex"}>
+      <span
+        className={
+          "transform -rotate-90 bg-black text-white py-5 px-5 text-3xl"
+        }
+      >
+        Xenovrs
+      </span>
+      <div className={"flex-grow "}>
+        <div className={"p-7 border-b border-b-black"}>
+          <span className={"inline-block transform -rotate-90"}>+ 3 (425) 521 60 21</span>
+        </div>
+        <div className={"flex justify-between"}>
+          <div>© Centrix 2021. All Rights Resevered</div>
+          <div>Privacy Policy</div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 const Layout = ({ children, className }: React.HTMLProps<HTMLDivElement>) => {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ function PresentSection({
   return (
     <section
       className={
-        "bg-black h-screen bg-cover bg-center grid grid-cols-12 grid-rows-3"
+        "bg-black min-h-screen bg-cover bg-center grid grid-cols-12 grid-rows-3"
       }
       style={{
         backgroundImage: `url(${imageUrl})`,
@@ -33,7 +33,9 @@ function Page() {
           <Header>Creative</Header>
           <Header>Color</Header>
           <Header>Cheme</Header>
-          <button className={"bg-yellow text-black px-20 py-2"}>SEE PROJECT</button>
+          <button className={"bg-yellow text-black px-20 py-2"}>
+            SEE PROJECT
+          </button>
         </div>
       </PresentSection>
       <PresentSection imageUrl={"https://source.unsplash.com/-v7EOw5SA4I"}>
@@ -42,6 +44,30 @@ function Page() {
           <Header>Creative</Header>
           <Header>Color</Header>
           <Header>Cheme</Header>
+        </div>
+      </PresentSection>
+      <PresentSection imageUrl={"https://source.unsplash.com/-v7EOw5SA4I"}>
+        <div
+          className={
+            "masonary row-span-3 col-span-12 text-white grid grid-cols-3 grid-rows-2 p-20 gap-3 h-screen"
+          }
+        >
+          <figure className={"row-span-2"}>
+            <img src={"https://source.unsplash.com/-v7EOw5SA4I"} />
+            <caption>Images Tell a Story</caption>
+          </figure>
+          <figure className={"md:row-span-1 md:col-start-2"}>
+            <img src={"https://source.unsplash.com/-v7EOw5SA4I"} />
+            <caption>Images Tell a Story</caption>
+          </figure>
+          <figure className={"md:row-span-1 md:col-start-3"}>
+            <img src={"https://source.unsplash.com/-v7EOw5SA4I"} />
+            <caption>Images Tell a Story</caption>
+          </figure>
+          <figure className={"md:col-start-2 md:col-span-2 md:row-start-2 md:row-end-3"}>
+            <img src={"https://source.unsplash.com/-v7EOw5SA4I"} />
+            <caption>Images Tell a Story</caption>
+          </figure>
         </div>
       </PresentSection>
     </Layout>
