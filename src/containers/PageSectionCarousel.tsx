d6ea97176eee63c46e12bc1fa4e61ecdaa6a5329
Axm <56112475+ax-sh@users.noreply.github.com>
2022-04-22 13:42:49 +0545

    feat: add component

--- src/containers/PageSectionCarousel.tsx
+++ src/containers/PageSectionCarousel.tsx
@@ -1,15 +1,34 @@
 import clsx from "clsx";
+import gsap from "gsap";
 import React from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import "./PageSectionCarousel.scss";
 
 function PageSectionCarousel({
   children,
   className,
   ...rest
 }: React.HTMLProps<HTMLDivElement>) {
-  const ref = React.useRef<HTMLDivElement>(null);
-  React.useLayoutEffect(() => {}, []);
+  const el = React.useRef<HTMLDivElement>(null);
+  const child = gsap.utils.selector(el);
+  // const tl = gsap.timeline();
+  gsap.registerPlugin(ScrollTrigger);
+  React.useLayoutEffect(() => {
+    child(".panel").forEach((panel) => {
+      gsap.from(panel.querySelector("h1"), {
+        x: 1000,
+        scrollTrigger: {
+          trigger: panel,
+          scroller: el.current,
+          markers: true,
+          scrub: true,
+        },
+      });
+    });
+  }, []);
+
   return (
-    <div ref={ref} className={clsx("PageSectionCarousel", className)} {...rest}>
+    <div ref={el} className={clsx("PageSectionCarousel", className)} {...rest}>
       {children}
     </div>
   );
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,11 +1,32 @@
 import Layout from "../layout";
+import PageSectionCarousel from "../containers/PageSectionCarousel";
 
 const BlogPage = () => {
   return (
     <Layout>
-      <section className={"h-screen text-white text-8xl bg-black grid place-items-center"}>
-          Blog
-      </section>
+      <PageSectionCarousel>
+        <section className=" panel red">
+          <h1 className="1">page one</h1>
+        </section>
+        <section className=" panel green">
+          <h1 className="2">page two</h1>
+        </section>
+        <section className=" panel yelow">
+          <h1 className="3">page tree</h1>
+        </section>
+        <section className=" panel blue">
+          <h1>page four</h1>
+        </section>
+        <section className=" panel pink">
+          <h1>page five</h1>
+        </section>
+        <section className=" panel turkey">
+          <h1>page six</h1>
+        </section>
+      </PageSectionCarousel>
+      {/*<section className={"h-screen text-white text-8xl bg-black grid place-items-center"}>*/}
+      {/*    Blog*/}
+      {/*</section>*/}
     </Layout>
   );
 };
