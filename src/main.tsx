e018ef37d6a43bb8aebfe1c07caac73697799355
Axm <56112475+ax-sh@users.noreply.github.com>
2022-04-22 16:52:54 +0545

    feat: react strict mode due to multi rendering

--- src/main.tsx
+++ src/main.tsx
@@ -22,9 +22,9 @@ const App = () => {
 };
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
