0c67254d7202c9791f4277ef1060c9c9e0c470ed
Axm <56112475+ax-sh@users.noreply.github.com>
2022-05-09 21:10:32 +0545

    wip

--- src/containers/PageSectionCarousel.tsx
+++ src/containers/PageSectionCarousel.tsx
@@ -1,6 +1,6 @@
 import clsx from "clsx";
 import gsap from "gsap";
-import React, { useLayoutEffect } from "react";
+import React, { useLayoutEffect, useRef } from "react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "./PageSectionCarousel.scss";
 // const tl = gsap.timeline();
@@ -14,33 +14,79 @@ const useRefsArray = () => {
       revealRefs.current.push(el);
     }
   }, []);
-  return [addToRefs, revealRefs];
+  return [addToRefs, revealRefs.current];
 };
 
-function gsapRun(current: HTMLDivElement[]) {
-  console.log(current, 8888);
-}
+function gsapRun(i: HTMLDivElement) {}
 
 function PageSectionCarousel({
   children,
   className,
   ...rest
 }: React.HTMLProps<HTMLDivElement>) {
-  const [addToRefs, refs] = useRefsArray();
+  const ref = useRef<HTMLDivElement>(null);
+  const [itemRef, sections] = useRefsArray();
 
   useLayoutEffect(() => {
-    gsapRun(refs.current);
+    // const trigger = ref.current;
+    const q = gsap.utils.selector(ref.current);
+    const trigger = q(".intro-trigger");
+    // const items = gsap.utils.toArray(".panel h1");
+    const shoutOut =
+      (message = "speechless") =>
+      () =>
+        console.log(message);
+
+    const showHideTrack = gsap.timeline({
+      onStart: shoutOut("showHideTrack start"),
+      onComplete: shoutOut("showHideTrack done"),
+    });
+    // .add(showMosaic)
+    // .add(buildMosaic, "<")
+    // .add(switchToDesktop, "-20%");
+
+    const zoomOut = gsap
+      .timeline({
+        onStart: shoutOut("zoomOut is starting"),
+        onComplete: shoutOut("🦀 zoomOut is done"),
+      })
+      .to(".red h1", { scale: 1, x: 88 }, "<");
+
+    const master = gsap
+      .timeline({
+        onStart: shoutOut("🟢 master start"),
+        onComplete: shoutOut("🏁 master done"),
+      })
+      // .add(showHideTrack)
+      .add(zoomOut);
+    // .add(zoomOut.duration(showHideTrack.duration()), "<");
+    console.log(zoomOut.duration(), )
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger,
+          start: "top top",
+          end: "+=500",
+          scrub: 1,
+          markers: true,
+          onEnter: () => console.log("header-enter"),
+          onLeave: () => console.log("header-leave"),
+          id: "intro-scene",
+        },
+      })
+      .add(master);
   }, []);
   return (
-    <div className={clsx("PageSectionCarousel", className)} {...rest}>
+    <div ref={ref} className={clsx("PageSectionCarousel", className)} {...rest}>
+      <div className="intro-trigger"></div>
       {React.Children.map(children, (child: any, index: number) =>
         React.cloneElement(child, {
-          ref: addToRefs,
           key: index,
+          ref: itemRef,
         })
       )}
     </div>
   );
 }
 
-export default PageSectionCarousel;
+export default React.memo(PageSectionCarousel);
